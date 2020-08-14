639c9719f4b9291055bd84e8506b07a790561e76
Sergei Shneider <shneider.s@gmail.com>
2020-08-14 19:27:54 -0400

    added button functionality, added trigger when paginating;

--- searchQueryFilter.js
+++ searchQueryFilter.js
@@ -8,6 +8,10 @@ let savedSettings = {
 };
 pageInitialization()
 function pageInitialization(){
+    loadData();
+    startUp();
+}
+function loadData(){
     try{
         const storageValue = JSON.parse(SQstorage.getItem("OMRCourseraSQ"))
         if(storageValue) savedSettings = {...storageValue}
@@ -16,45 +20,49 @@ function pageInitialization(){
         SQstorage.removeItem("OMRCourseraSQ")
     }
 }
-
+function startUp(){
+    let CourseCards = document.getElementsByClassName("rc-DesktopSearchCard")
+    addButtons(CourseCards)
+    filterResults(CourseCards)
+}
 function filterResults(CourseCards){
     console.log("filterResults Triggered")
     console.log(savedSettings)
+    console.log(CourseCards)
+    const cardsToRemove = [];
     for (let i = 0; i<CourseCards.length; i++){
         console.log(CourseCards[i].href)
         if(CourseCards[i].href in savedSettings.saved){
             CourseCards[i].parentNode.style.border = "8px solid green"
         }
-        else if(CourseCards[i].href in savedSettings.removed){
-            CourseCards[i].remove()
+        else if(CourseCards[i].href in savedSettings.removed || CourseCards[i].href in savedSettings.done){
+            cardsToRemove.push(CourseCards[i])
         }
+    }
+    for(let i = 0; i<cardsToRemove.length; i++){
+        cardsToRemove[i].remove();
     }
 }
 
-// function generateFilterButton(){
-//     const AppButton = document.createElement("BUTTON")
-//     AppButton.innerHTML = "Start Filtering"
-//     AppButton.id = "searchQueryToggleButton"
-//     AppButton.addEventListener("click", addButtons)
-//     let buttonAnchor = document.getElementsByClassName("rc-NumberOfResultsSection")
-//     buttonAnchor[0].appendChild(AppButton)
-// }
 const observer = new MutationObserver(mutations => {
+    //console.log(mutations)
     for(let mutation of mutations){
-        for (let addedNode of mutation.addedNodes){
-          if(addedNode.className == "rc-CourseraPlusSearchFilterOption"){
-              //generateFilterButton()
-              let CourseCards = document.getElementsByClassName("rc-DesktopSearchCard")
-              addButtons(CourseCards)
-              filterResults(CourseCards)
+    //    console.log(mutation.target.className)
+        if(mutation.target.className === "rc-Suggestion" ){
+            startUp();
+            break;
           }
-        }
+        // for (let addedNode of mutation.addedNodes){
+           
+        //   if(addedNode.className == "rc-Suggestion" ){
+        //     startUp();
+        //   }
+        // }
     }
 })
 observer.observe(document, { childList: true, subtree: true });
 function addButtons(CourseCards){
     console.log("addButtonsTriggered")
-    
     for(let i = 0; i<CourseCards.length; i++){
         let buttonDiv = createButtonDiv()
         CourseCards[i].appendChild(buttonDiv)
@@ -80,16 +88,16 @@ function saveForLater(e){
     parentElement.parentNode.style.border = "8px solid green"
     saveToLocalStorage(parentElement.href, "saved")
 }
-function notInterested(e){
+function notInterestedOrCompleted(e, sourceIn=0){
     e.preventDefault()
     e.stopPropagation()
     const parentElement = getParentElement(e)
-    saveToLocalStorage(parentElement.href, "removed")
+    let destination = "removed";
+    if(sourceIn) destination = "done"
+    saveToLocalStorage(parentElement.href, destination)
     parentElement.remove()
 }
-function alreadyCompleted(e){
-    return 0
-}
+
 /*FILTER BUTTON GENERATORS START*/
 function generateSaveForLater(){
     let saveForLaterButton = document.createElement("BUTTON")
@@ -102,14 +110,14 @@ function generateNotInterestedButton(){
     let notInterestedButton = document.createElement("BUTTON")
     notInterestedButton.className = "removeButton"
     notInterestedButton.innerHTML = "\u2716"
-    notInterestedButton.addEventListener("click", notInterested)
+    notInterestedButton.addEventListener("click", notInterestedOrCompleted)
     return notInterestedButton;
 }
 function generateAlreadyCompletedButton(){
     let alreadyCompletedButton  = document.createElement("BUTTON")
     alreadyCompletedButton.className = "completedButton"
     alreadyCompletedButton.innerHTML = "\u2713"
-    alreadyCompletedButton.addEventListener("click", alreadyCompleted)
+    alreadyCompletedButton.addEventListener("click", ()=>{notInterestedOrCompleted(event, 1)})
     return alreadyCompletedButton
 }
 /*FILTER BUTTON GENERATORS END*/
